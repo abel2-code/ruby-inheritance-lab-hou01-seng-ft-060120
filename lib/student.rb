cdadad0ea82f95dfb723bf0d38e4681525e4a4d6
Abelardo Miranda <available_email_address@yahoo.com>
2020-06-05 00:01:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+  attr_accessor :learn, :knowledge
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(material)
+    @learn = material
+    @knowledge.push(@learn)
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
